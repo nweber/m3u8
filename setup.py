bafa888ac8f634f91af1fd4b67e149774788b9b2
Nathan Weber <nweber@dpitdev48.digitalprimates.local>
2017-01-17 08:07:29 -0600

    Add requirements to setup.

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from os.path import dirname, abspath, join, exists
 from setuptools import setup
 
 long_description = None
-if exists("README.rst"):
-    with  open("README.rst") as file:
+if exists('README.rst'):
+    with  open('README.rst') as file:
         long_description = file.read()
 
 install_reqs = [req for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
 
 setup(
-    name="m3u8",
+    name='m3u8',
     author='Globo.com',
     author_email='videos3@corp.globo.com',
-    version="0.3.0b",
+    version='0.3.0b',
     license='MIT',
     zip_safe=False,
     include_package_data=True,
     install_requires=install_reqs,
-    packages=["m3u8"],
-    url="https://github.com/nweber/m3u8",
-    download_url = 'https://github.com/nweber/m3u8/tarball/0.3.0b'
-    description="Python m3u8 parser",
+    packages=['m3u8'],
+    url='https://github.com/nweber/m3u8',
+    download_url = 'https://github.com/nweber/m3u8/tarball/0.3.0b',
+    description='Python m3u8 parser',
     long_description=long_description
     )
